3e92e928cde868d6bbba1b2c8547155e452c02a1
Aqil321821 <aqilrao321821@gmail.com>
2025-08-11 19:22:14 +0500

    lego search done

--- lego/script.js
+++ lego/script.js
@@ -4,3 +4,35 @@ var swiper = new Swiper('.mySwiper', {
     prevEl: '.swiper-button-prev',
   },
 });
+
+let inpSearch = false;
+const searchInput = document.getElementById('search-items');
+const searchIcon = document.getElementById('search-icon');
+
+function openSearch() {
+  if (inpSearch === false) {
+    document.getElementById('btnSearch').classList.add('d-none');
+    document.getElementById('inpSearch').classList.remove('d-none');
+    document.getElementById(
+      'box-shadow'
+    ).innerHTML = `<div onclick="openSearch()" class="modal-backdrop fade show z-0"></div>`;
+    searchInput.value = '';
+    inpSearch = true;
+  } else {
+    document.getElementById('btnSearch').classList.remove('d-none');
+    document.getElementById('inpSearch').classList.add('d-none');
+    document.getElementById('box-shadow').innerHTML = ``;
+    searchInput.value = '';
+    searchInput.value ? (searchIcon.style.display = 'none') : (searchIcon.style.display = 'inline-block');
+
+    inpSearch = false;
+  }
+}
+
+searchInput.addEventListener('input', () => {
+  if (searchInput.value.trim().length > 0) {
+    searchIcon.style.display = 'none';
+  } else {
+    searchIcon.style.display = 'inline-block';
+  }
+});
