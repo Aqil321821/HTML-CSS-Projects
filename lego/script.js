158c62ce4b7f0093502439821ad7e53a95251bea
Aqil321821 <aqilrao321821@gmail.com>
2025-08-13 02:03:09 +0500

    nav resp & animation menu done

--- lego/script.js
+++ lego/script.js
@@ -36,3 +36,31 @@ searchInput.addEventListener('input', () => {
     searchIcon.style.display = 'inline-block';
   }
 });
+
+function closeTopbar() {
+  let toppestDiv = document.getElementById('toppest-div');
+  if (!toppestDiv.classList.contains('d-none')) {
+    toppestDiv.classList.add('d-none');
+  }
+}
+
+window.addEventListener('resize', () => {
+  let toppestDiv = document.getElementById('toppest-div');
+  if (toppestDiv.classList.contains('d-none')) {
+    if (window.innerWidth >= 992) {
+      toppestDiv.classList.remove('d-none');
+    }
+  }
+});
+
+// Bars animation
+function animateBars() {
+  console.log('animate function');
+  let bar_1 = document.querySelector('.icon-bar-1');
+  let bar_2 = document.querySelector('.icon-bar-2');
+  let bar_3 = document.querySelector('.icon-bar-3');
+
+  bar_1.classList.toggle('icon-bar-1-active');
+  bar_2.classList.toggle('icon-bar-2-active');
+  bar_3.classList.toggle('icon-bar-3-active');
+}
