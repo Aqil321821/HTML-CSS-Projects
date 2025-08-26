dd0e57ca9caedba625c357745908f1eb405c991a
Aqil321821 <aqilrao321821@gmail.com>
2025-08-26 10:10:36 +0500

    sidebar done

--- Traders-Joe/app.js
+++ Traders-Joe/app.js
@@ -1,7 +1,17 @@
 let hamBtn = document.getElementById('ham-btn');
 let crossBtn = document.getElementById('corss-btn');
+let offCanvas = document.getElementById('my-menu-offcanvas1');
+let myNav = document.getElementById('myNav');
 
 hamBtn.addEventListener('click', () => {
   hamBtn.classList.add('d-none');
-  crossBtn.classList.add('d-block');
+  crossBtn.classList.remove('d-none');
+  offCanvas.classList.add('active');
+  myNav.classList.add('bg-nav-cream');
+});
+crossBtn.addEventListener('click', () => {
+  crossBtn.classList.add('d-none');
+  hamBtn.classList.remove('d-none');
+  offCanvas.classList.remove('active');
+  myNav.classList.remove('bg-nav-cream');
 });
