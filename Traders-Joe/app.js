c3fea06fd0f4f106a9469c6e3084d950b31dbeb4
Aqil321821 <aqilrao321821@gmail.com>
2025-08-26 11:11:47 +0500

    added mobile off canvas menu

--- Traders-Joe/app.js
+++ Traders-Joe/app.js
@@ -1,5 +1,10 @@
-let hamBtn = document.getElementById('ham-btn');
-let crossBtn = document.getElementById('corss-btn');
+let hamBtn = document.getElementById('ham-btn-desk');
+let crossBtn = document.getElementById('corss-btn-desk');
+
+let hamBtnMob = document.getElementById('ham-btn-mob');
+let crossBtnMob = document.getElementById('corss-btn-mob');
+let mobOffCanvas = document.getElementById('off-mobile');
+
 let offCanvas = document.getElementById('my-menu-offcanvas1');
 let myNav = document.getElementById('myNav');
 
@@ -13,5 +18,17 @@ crossBtn.addEventListener('click', () => {
   crossBtn.classList.add('d-none');
   hamBtn.classList.remove('d-none');
   offCanvas.classList.remove('active');
+  myNav.classList.remove('bg-nav-cream');
+});
+hamBtnMob.addEventListener('click', () => {
+  hamBtnMob.classList.add('d-none');
+  crossBtnMob.classList.remove('d-none');
+  mobOffCanvas.classList.add('active');
+  myNav.classList.add('bg-nav-cream');
+});
+crossBtnMob.addEventListener('click', () => {
+  crossBtnMob.classList.add('d-none');
+  hamBtnMob.classList.remove('d-none');
+  mobOffCanvas.classList.remove('active');
   myNav.classList.remove('bg-nav-cream');
 });
